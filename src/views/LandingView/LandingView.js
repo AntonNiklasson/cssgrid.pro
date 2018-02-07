e5fd7360b40fd80e2e170269d0c062cc5b597a74
Anton Niklasson <niklasson.anton@gmail.com>
2018-02-07 13:04:55 +0100

    mvp for the landing page

--- src/views/LandingView/LandingView.js
+++ src/views/LandingView/LandingView.js
@@ -4,16 +4,23 @@ import glamorous from 'glamorous';
 const Wrapper = glamorous.div({
   width: '100vw',
   height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
   background: 'whitesmoke',
   color: '#333',
+
+  '& h1': {
+    margin: '0 0 1em 0',
+  },
 });
 
-const LandingView = () => {
-  return (
-    <Wrapper>
-      <h1>CSS Grid</h1>
-    </Wrapper>
-  );
-};
+const LandingView = () => (
+  <Wrapper>
+    <h1>Learn CSS Grid</h1>
+    <h2>Soon.</h2>
+  </Wrapper>
+);
 
 export default LandingView;

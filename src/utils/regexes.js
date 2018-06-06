403629599b2eecaf008f07cfe7cd08d6ab490a31
Anton Niklasson <niklasson.anton@gmail.com>
2018-06-06 15:13:11 +0200

    add missing deps

--- src/utils/regexes.js
+++ src/utils/regexes.js
@@ -3,11 +3,11 @@
  *
  * (pixels, percentage, fr, em, rem)
  */
-export const moreThanOneLengthParameter = /^(-?[0-9]*\.?[0-9]+\s?(em|fr|px|%)\s*){2,}$/
+export const moreThanOneLengthParameter = /^(-?[0-9]*\.?[0-9]+\s?(em|fr|px|%)\s*){2,};?$/;
 
 /**
  * Match one or two length parameters.
  *
  * (pixels, percentage, fr, em, rem)
  */
-export const oneOrTwoLengthParameters = /^(-?[0-9]*\.?[0-9]+\s?(em|fr|px|%)\s*){1,2}$/
+export const oneOrTwoLengthParameters = /^(-?[0-9]*\.?[0-9]+\s?(em|fr|px|%)\s*){1,2};?$/;

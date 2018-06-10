fbc91644e3606b2c8fed7721f8bba22c459fb4a1
Anton Niklasson <niklasson.anton@gmail.com>
2018-06-10 14:02:34 +0200

    feat: slight design touchup on the output

--- src/views/ChallengeView/Output.js
+++ src/views/ChallengeView/Output.js
@@ -1,35 +1,37 @@
-import React, { Component } from 'react'
-import glamorous from 'glamorous'
+import React, { Component } from "react";
+import glamorous from "glamorous";
 
 const Container = glamorous.div(({ theme }) => ({
-  gridArea: 'output',
-  padding: '1em',
-  fontSize: '30px',
-  background: '#CCC',
-  userSelect: 'none',
-  overflow: 'auto',
+  gridArea: "output",
+  padding: "1em",
+  fontSize: "30px",
+  background: "#CCC",
+  backgroundImage: "linear-gradient(#ffaeae, #ff99e9)",
+  borderTop: `1px solid ${theme.colors.gray}`,
+  userSelect: "none",
+  overflow: "auto",
 
-  '& .grid': {
-    display: 'grid',
-    width: '100%',
+  "& .grid": {
+    display: "grid",
+    width: "100%",
     background: theme.colors.primaryLight,
-    transition: 'all 500ms',
+    transition: "all 500ms"
   },
-  '& .grid > *': {
-    padding: '.2em',
-    background: theme.colors.accentLight,
-    transition: 'all 300ms',
-  },
-}))
+  "& .grid > *": {
+    padding: ".2em",
+    border: `3px solid ${theme.colors.primary}`,
+    transition: "all 300ms"
+  }
+}));
 
 class Output extends Component {
   renderMarkup = () => ({
-    __html: this.props.markup,
-  })
+    __html: this.props.markup
+  });
 
   render() {
-    return <Container dangerouslySetInnerHTML={this.renderMarkup()} />
+    return <Container dangerouslySetInnerHTML={this.renderMarkup()} />;
   }
 }
 
-export default Output
+export default Output;
